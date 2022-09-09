b300c188aa8030cbaf8252f5cc2bcc9052226635
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-09 16:11:30 +0700

    add login route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Chat\ChatController;
+use App\Http\Controllers\API\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->as('v1')->group(function () {
+    Route::get('test-connection', function () {
+        return response()->json(
+            [
+                'status' =>  'Connection established',
+            ]
+        );
+    });
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [LoginController::class, 'login']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        
+    });
 });

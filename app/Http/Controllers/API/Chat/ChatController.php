7f73b5f9ec7cd6056753c8d55619ebd5041c3351
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 16:14:06 +0700

    add chat controller

--- app/Http/Controllers/API/Chat/ChatController.php
+++ app/Http/Controllers/API/Chat/ChatController.php
@@ -3,47 +3,52 @@
 namespace App\Http\Controllers\API\Chat;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Chat\SendingChatRequests;
 use App\Http\Resources\ChatRoom\ChatRoomResource;
 use App\Http\Resources\User\UserResource;
 use App\Models\ChatRoom;
+use App\Models\User;
+use App\Services\ChatService;
+use App\Services\GroupChatService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     //
-    public function index()
+    public function index(GroupChatService $groupChatService)
     {
         $user = auth()->user();
-        $chatRooms = ChatRoom::query()
-            ->whereRelation('users', 'users.id', $user->id)
-            ->with(
-                [
-                    'chats'    =>  function ($query) {
-                        $query->orderBy('created_at', 'desc')
-                            ->with(
-                                [
-                                    'user'
-                                ]
-                            )
-                            ->first();
-                    }
-                ]
-            )
-            ->withCount(
-                [
-                    'chats as unread_chats' =>  function ($query) {
-                        $query->where('created_at', '>', 'chat_room_user.last_opened');
-                    }
-                ]
-            )
-            ->get()
-            //
-        ;
+        $chatRooms = $groupChatService->list();
         return response()->json(
             [
                 'user'          =>  new UserResource($user),
                 'chatRooms'     =>  ChatRoomResource::collection($chatRooms),
             ]
         );
+    }
+
+    public function user(UserService $userService)
+    {
+        $users = $userService->index();
+        return response()->json(
+            [
+                'users' =>  UserResource::collection($users),
+            ]
+        );
+    }
+
+    public function sendToUser(SendingChatRequests $request, UserService $userService, User $user)
+    {
+        $validated = $request->validated();
+        $chat = null;
+        if ($userService->checkIfChatRoomIsAvailable($user)) {
+        } else {
+        }
+
+        return response()->json(
+            $userService->checkIfChatRoomIsAvailable($user)
+        );
+        $chat = $userService->sendMessage($user, $validated);
     }
 }

a07c68c19ec8b7b0b96a1715535820efe1419a0f
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 17:23:16 +0700

    finish showing chatRoom messages and sending message for chatRoom

--- app/Http/Controllers/API/Chat/ChatRoomController.php
+++ app/Http/Controllers/API/Chat/ChatRoomController.php
@@ -8,15 +8,15 @@ use App\Http\Resources\Chat\ChatResource;
 use App\Http\Resources\ChatRoom\ChatRoomResource;
 use App\Models\ChatRoom;
 use App\Services\ChatService;
-use App\Services\GroupChatService;
+use App\Services\ChatRoomService;
 use Illuminate\Http\Request;
 
 class ChatRoomController extends Controller
 {
     //
-    public function index(ChatRoom $chatRoom, GroupChatService $groupChatService)
+    public function index(ChatRoom $chatRoom, ChatRoomService $chatRoomService)
     {
-        $chatRoom = $groupChatService->index($chatRoom);
+        $chatRoom = $chatRoomService->index($chatRoom);
         return response()->json(
             [
                 'chatRoom'  =>  new ChatRoomResource($chatRoom),
@@ -27,7 +27,12 @@ class ChatRoomController extends Controller
     public function send(SendingChatRequests $request, ChatRoom $chatRoom, ChatService $ChatService)
     {
         $validated = $request->validated();
-        $chat = $ChatService->sendMessage($chatRoom, $validated);
+        $chat = null;
+        if ($ChatService->isTextIsEmpty($validated)) {
+            $chat = $ChatService->nullChat();
+        } else {
+            $chat = $ChatService->sendMessage($chatRoom, $validated);
+        }
         return response()->json(
             // $chat
             [

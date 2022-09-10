c72217200d4b8530ec0f415f0e1333cb80303011
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 17:27:04 +0700

    removing sending message from user service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -8,40 +8,19 @@ use App\Models\ChatRoom;
 
 class UserService
 {
-    public function index()
+    public function list()
     {
         $users = User::where('id', '!=', auth()->id())->get();
         $users->map(function ($user) {
-            $user = $this->loadUserDirectMessage($user);
+            $user->directMessage = $this->loadUserDirectMessage($user);
         });
         return $users;
     }
 
-    public function sendMessage(User $user, array $validated)
+    public function isDirectMessageExists(User $user)
     {
-        $chat = null;
-
-        return $this->storeChat($user, $validated);
-    }
-
-    public function checkIfChatRoomIsAvailable(User $user)
-    {
-        $user = $this->loadUserDirectMessage($user);
-        return $user->directMessage != null ? true : false;
-    }
-
-    public function sendMessageToExistingChatRoom(User $user, array $validated)
-    {
-        
-    }
-
-    public function storeChat(User $user, array $validated)
-    {
-        $chat = Chat::make($validated);
-        $chat->chat_room_id = $chatRoom->id;
-        $chat->user_id = auth()->id();
-        $chat->save();
-        return $chat;
+        $chatRoom = $this->loadUserDirectMessage($user);
+        return $chatRoom != null ? true : false;
     }
 
     public function nullChat()
@@ -56,7 +35,7 @@ class UserService
             ->whereRelation('users', 'chat_room_user.user_id', $user->id)
             ->whereRelation('users', 'chat_room_user.user_id', auth()->id())
             ->first();
-        $user->directMessage = $chatRoom;
-        return $user;
+        return $chatRoom;
+        // return $user;
     }
 }

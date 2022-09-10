3bf31734d6fc221258a19c04a694e4a0007fe71a
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 16:15:16 +0700

    make default value of user's last seen as current time

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->timestamp('last_seen')->nullable();
+            $table->timestamp('last_seen')->default(now());
             $table->rememberToken();
             $table->timestamps();
         });

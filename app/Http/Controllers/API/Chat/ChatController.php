d312a287a45818a4445d061ff864380718de10e3
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 13:27:09 +0700

    create list of chatroom

--- app/Http/Controllers/API/Chat/ChatController.php
+++ app/Http/Controllers/API/Chat/ChatController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\Chat;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ChatRoom\ChatRoomResource;
 use App\Http\Resources\User\UserResource;
 use App\Models\ChatRoom;
 use Illuminate\Http\Request;
@@ -15,13 +16,33 @@ class ChatController extends Controller
         $user = auth()->user();
         $chatRooms = ChatRoom::query()
             ->whereRelation('users', 'users.id', $user->id)
+            ->with(
+                [
+                    'chats'    =>  function ($query) {
+                        $query->orderBy('created_at', 'desc')
+                            ->with(
+                                [
+                                    'user'
+                                ]
+                            )
+                            ->first();
+                    }
+                ]
+            )
+            ->withCount(
+                [
+                    'chats as unread_chats' =>  function ($query) {
+                        $query->where('created_at', '>', 'chat_room_user.last_opened');
+                    }
+                ]
+            )
             ->get()
             //
         ;
         return response()->json(
             [
                 'user'          =>  new UserResource($user),
-                'chatRooms'     =>  $chatRooms
+                'chatRooms'     =>  ChatRoomResource::collection($chatRooms),
             ]
         );
     }

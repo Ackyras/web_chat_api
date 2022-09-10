08bd3eead14cb6dee8ae70ae7c2d1b9e301ccb31
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 13:27:42 +0700

    create endpoint for list of chatroom, list of chat, and sending message

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Chat\ChatController;
 use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Chat\ChatRoomController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,8 +33,18 @@ Route::prefix('v1')->as('v1')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::prefix('chat')->as('chat.')->group(function () {
             Route::controller(ChatController::class)->group(function () {
-                Route::get('/', 'index');
-                // Route::post('/orders', 'store');
+                Route::get('/', 'index')->name('index');
+            });
+            Route::middleware(['isUserMemberOfChatRoom'])->controller(ChatRoomController::class)->prefix('room')->as('room.')->group(function () {
+                Route::get('/{chatRoom}', 'index')->name('index')->missing(function () {
+                    return response()->json(
+                        [
+                            'msg'   =>  'You have never started a conversation in this room',
+                        ],
+                        404
+                    );
+                });
+                Route::post('/{chatRoom}', 'send')->name('send');
             });
         });
     });

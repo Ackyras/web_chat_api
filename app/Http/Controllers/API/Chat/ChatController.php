013fb1722ff8b617a9511202440a020ed13713e3
Ackyras <45270846+Ackyras@users.noreply.github.com>
2022-09-10 17:22:50 +0700

    finish list of chatRoom function (index of app)

--- app/Http/Controllers/API/Chat/ChatController.php
+++ app/Http/Controllers/API/Chat/ChatController.php
@@ -9,46 +9,20 @@ use App\Http\Resources\User\UserResource;
 use App\Models\ChatRoom;
 use App\Models\User;
 use App\Services\ChatService;
-use App\Services\GroupChatService;
+use App\Services\ChatRoomService;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     //
-    public function index(GroupChatService $groupChatService)
+    public function index(ChatRoomService $chatRoomService)
     {
-        $user = auth()->user();
-        $chatRooms = $groupChatService->list();
+        $chatRooms = $chatRoomService->list();
         return response()->json(
             [
-                'user'          =>  new UserResource($user),
                 'chatRooms'     =>  ChatRoomResource::collection($chatRooms),
             ]
         );
-    }
-
-    public function user(UserService $userService)
-    {
-        $users = $userService->index();
-        return response()->json(
-            [
-                'users' =>  UserResource::collection($users),
-            ]
-        );
-    }
-
-    public function sendToUser(SendingChatRequests $request, UserService $userService, User $user)
-    {
-        $validated = $request->validated();
-        $chat = null;
-        if ($userService->checkIfChatRoomIsAvailable($user)) {
-        } else {
-        }
-
-        return response()->json(
-            $userService->checkIfChatRoomIsAvailable($user)
-        );
-        $chat = $userService->sendMessage($user, $validated);
     }
 }
